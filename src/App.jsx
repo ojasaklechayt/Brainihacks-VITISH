e4425036d66d7f7b041e13e9a8b25ac09d6cf5f5
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-03-25 09:00:51 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,33 @@ import Model from './components/Model/Model';
 import Video1 from './components/Video1/video1';
 import Video2 from './components/Video2/video2';
 import Login from './components/Login/Login';
-
+import { useEffect, useState } from 'react';
+import { ClimbingBoxLoader } from 'react-spinners'
+;
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3500)
+  }, [])
+
   return (
     <>
-        <div>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="/login" element={<Login />} />            
-              <Route path="/model" element={<Model />} />
-              <Route path="/videoone" element={<Video1 />} /> 
-              <Route path="/videotwo" element={<Video2 />} /> 
-            </Routes>
-          </BrowserRouter>
-        </div>
+      <div>{loading ?
+        <ClimbingBoxLoader style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} color="#7c36d6" /> :
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/model" element={<Model />} />
+            <Route path="/videoone" element={<Video1 />} />
+            <Route path="/videotwo" element={<Video2 />} />
+          </Routes>
+        </BrowserRouter>}
+      </div>
     </>
   )
 }
