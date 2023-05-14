3c014077d10e861523897c8c0fbe4e89cead198d
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-05-14 16:51:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// Import necessary modules from React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+// Import the App component and the index.css stylesheet
+import App from './App';
+import './index.css';
+
+// Use ReactDOM to render the App component to the root element of the HTML document
+// wrapped in a <React.StrictMode> element for additional checks and warnings
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
