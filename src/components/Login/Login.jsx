fbaf4ba3814e928e9240e0fd9a5ce1a98d3fc69b
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-05-16 00:31:03 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,16 @@
+// Import the React library
 import React from "react";
 
+// Define the Login component
 const Login = () => {
+    // Return a div
     return (
         <div>
+            // Return a h1 with the text Login
             <h1>Login</h1>
         </div>
     );
 };
 
+// Export the Login component
 export default Login;
