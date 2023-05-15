6c78ec0b9e7871d793a3a7418b577e443abf2e27
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-05-16 00:35:13 +0530

    Update Model.jsx

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import styles from './Model.module.css';
-import { motion } from 'framer-motion';
+import React from 'react'; /* Import the React library */
+import { useNavigate } from 'react-router'; /* Import the useNavigate hook from the react-router library */
+import styles from './Model.module.css'; /* Import the CSS styles for this component */
+import { motion } from 'framer-motion'; /* Import the motion component from the framer-motion library */
 
 const Model = () => {
 
-    const MotionHeading = motion.div;
+    const MotionHeading = motion.div; /* Create a new variable called MotionHeading that is equal to the motion.div component */
 
-    const navigate = useNavigate();
+    const navigate = useNavigate(); /* Create a new navigate variable using the useNavigate hook */
 
     const navigatevideo1 = () => {
-        navigate('/videoone');
+        navigate('/videoone'); /* Define a function that navigates to the /videoone route when called */
     }
 
     const navigatevideo2 = () => {
-        navigate('/videotwo');
+        navigate('/videotwo'); /* Define a function that navigates to the /videotwo route when called */
     }
 
     return (
-        <div className={styles.wrapper_outside}>
-            <div className={styles.wrapper}>
+        <div className={styles.wrapper_outside}> /* Use the wrapper_outside class from the imported CSS styles */
+            <div className={styles.wrapper}> /* Use the wrapper class from the imported CSS styles */
                 <MotionHeading
-                    initial={{ opacity: 0, y: -100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, delay: 0.5 }}>
-                    <div className={styles.card1}>
-                        <h2>Automatic Numberplate Recognisation System</h2>
+                    initial={{ opacity: 0, y: -100 }} /* Define the initial state of the animation */
+                    animate={{ opacity: 1, y: 0 }} /* Define the end state of the animation */
+                    transition={{ duration: 1, delay: 0.5 }}> /* Define the duration and delay of the animation */
+                    <div className={styles.card1}> /* Use the card1 class from the imported CSS styles */
+                        <h2>Automatic Numberplate Recognisation System</h2> /* Display a heading for the first card */
                         <p>Automatic Number Plate Recognition (ANPR) or Automatic License Plate Recognition (ALPR) is a
                             technology that uses optical character recognition (OCR) and pattern recognition software to read
                             vehicle registration plates. ANPR systems typically use cameras to capture images of vehicle
@@ -34,18 +34,18 @@ const Model = () => {
                             identifying stolen vehicles, tracking traffic violations, or managing access to restricted areas.
                             ANPR technology has become increasingly widespread and is now commonly used by law enforcement
                             agencies, parking enforcement companies, toll operators, and other organizations that need to
-                            monitor vehicle movements.</p>
-                        <div className={styles.button}>
-                            <button onClick={navigatevideo1}>View</button>
+                            monitor vehicle movements.</p> /* Display a paragraph of text for the first card */
+                        <div className={styles.button}> /* Use the button class from the imported CSS styles */
+                            <button onClick={navigatevideo1}>View</button> /* Display a button that triggers the navigatevideo1 function when clicked */
                         </div>
                     </div>
                 </MotionHeading>
                 <MotionHeading
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, delay: 0.5 }}>
-                    <div className={styles.card2}>
-                        <h2>Facial Recognisation System</h2>
+                    initial={{ opacity: 0, y: 100 }} /* Define the initial state of the animation */
+                    animate={{ opacity: 1, y: 0 }} /* Define the end state of the animation */
+                    transition={{ duration: 1, delay: 0.5 }}> /* Define the duration and delay of the animation */
+                    <div className={styles.card2}> /* Use the card2 class from the imported CSS styles */
+                        <h2>Facial Recognisation System</h2> /* Display a heading for the second card */
                         <p>Facial recognition technology is a type of biometric system that uses algorithms to identify and
                             authenticate individuals based on their facial features. The technology works by analyzing and
                             comparing unique facial features, such as the distance between the eyes, nose, and mouth, to a
@@ -60,9 +60,9 @@ const Model = () => {
 
                             While facial recognition technology has many potential benefits, such as improving security and
                             convenience, it also raises significant privacy and ethical concerns. These include issues related
-                            to data protection, surveillance, and potential biases in the algorithms used.</p>
-                        <div className={styles.button}>
-                            <button onClick={navigatevideo2}>View</button>
+                            to data protection, surveillance, and potential biases in the algorithms used.</p> /* Display a paragraph of text for the second card */
+                        <div className={styles.button}> /* Use the button class from the imported CSS styles */
+                            <button onClick={navigatevideo2}>View</button> /* Display a button that triggers the navigatevideo2 function when clicked */
                         </div>
                     </div>
                 </MotionHeading>
@@ -71,4 +71,4 @@ const Model = () => {
     )
 }
 
-export default Model
+export default Model /* Export the Model component for use in other parts of the application */
