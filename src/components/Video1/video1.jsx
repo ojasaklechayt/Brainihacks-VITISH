2e4f8f87945a1702b023ab934ca14adbb34ce7d0
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-05-16 00:37:22 +0530

    Update video1.jsx

--- src/components/Video1/video1.jsx
+++ src/components/Video1/video1.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import videoone from "../../assets/output.gif";
-import videotwo from "../../assets/output_final.gif";
-import styles from "./video1.module.css";
-import { motion } from "framer-motion";
+import React from "react"; // Import the React library
+import videoone from "../../assets/output.gif"; // Import the output.gif file as a variable named "videoone"
+import videotwo from "../../assets/output_final.gif"; // Import the output_final.gif file as a variable named "videotwo"
+import styles from "./video1.module.css"; // Import the CSS module for the Video1 component
+import { motion } from "framer-motion"; // Import the motion library from Framer Motion
 
-const Video1 = () => {
-    const MotionHeading = motion.div;
+const Video1 = () => { // Define a functional component named Video1
+    const MotionHeading = motion.div; // Create a new variable named MotionHeading that is a motion-enhanced div
 
     return (
-        <div className={styles.wrapper_top}>
-            <div className={styles.wrapper}>
-                <div className={styles.text}>
-                    <h1>A. N. P. R.</h1>
+        <div className={styles.wrapper_top}> // Define a div with a class name of "wrapper_top"
+            <div className={styles.wrapper}> // Define a div with a class name of "wrapper"
+                <div className={styles.text}> // Define a div with a class name of "text"
+                    <h1>A. N. P. R.</h1> // Define a level one heading with the text "A. N. P. R."
                 </div>
-                <MotionHeading
-                    initial={{ opacity: 0, y: -100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.4, delay: 0.5 }}
+                <MotionHeading // Define a motion-enhanced div using the MotionHeading variable
+                    initial={{ opacity: 0, y: -100 }} // Set the initial state of the div to have an opacity of 0 and a y-axis position of -100
+                    animate={{ opacity: 1, y: 0 }} // Set the animated state of the div to have an opacity of 1 and a y-axis position of 0
+                    transition={{ duration: 0.4, delay: 0.5 }} // Set the duration of the animation to 0.4 seconds and delay it by 0.5 seconds
                 >
-                    <div className={styles.video_out}>
-                        <div className={styles.video}>
-                            <h3>Numberplate Detection</h3>
-                            <img src={videoone} alt="videoone" />
+                    <div className={styles.video_out}> // Define a div with a class name of "video_out"
+                        <div className={styles.video}> // Define a div with a class name of "video"
+                            <h3>Numberplate Detection</h3> // Define a level three heading with the text "Numberplate Detection"
+                            <img src={videoone} alt="videoone" /> // Render an image with the source of the "videoone" variable and an alt tag of "videoone"
                         </div>
-                        <div className={styles.video}>
-                            <h3>Numberplate Recognition</h3>
-                            <img src={videotwo} alt="videotwo" />
+                        <div className={styles.video}> // Define a div with a class name of "video"
+                            <h3>Numberplate Recognition</h3> // Define a level three heading with the text "Numberplate Recognition"
+                            <img src={videotwo} alt="videotwo" /> // Render an image with the source of the "videotwo" variable and an alt tag of "videotwo"
                         </div>
                     </div>
                 </MotionHeading>
@@ -34,4 +34,4 @@ const Video1 = () => {
     );
 };
 
-export default Video1;
+export default Video1; // Export the Video1 component as the default export
